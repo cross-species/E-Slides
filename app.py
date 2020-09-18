2f753145d10c28452fb918de34956844af852cd6
梅佳奕 <10165300206@stu.ecnu.edu.cn>
2020-09-18 17:05:42 +0800

    merge front-end

--- app.py
+++ app.py
@@ -3,32 +3,42 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods=('POST'))
+@app.route('/',methods=('GET', 'POST'))
 def login():
-    username = request.form['username']
-    password = request.form['password']
-    # db = get_db()
-    # error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # db = get_db()
+        # error = None
 
-    if not username:
-        error = 'Username is required.'
-    elif not password:
-        error = 'Password is required.'
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        # elif db.execute(
+        #     'SELECT id FROM user WHERE username = ?', (username,)
+        # ).fetchone() is not None:
+        #     error = 'User {} is already registered.'.format(username)
 
-    ##############不管用户名密码是什么都login#############
-    # elif db.execute(
-    #     'SELECT id FROM user WHERE username = ?', (username,)
-    # ).fetchone() is not None:
-    #     error = 'User {} is already registered.'.format(username)
+        # if error is None:
+        #     db.execute(
+        #         'INSERT INTO user (username, password) VALUES (?, ?)',
+        #         (username, generate_password_hash(password))
+        #     )
+        #     db.commit()
+        else:
+            return redirect(url_for('home'))
 
-    # if error is None:
-    #     db.execute(
-    #         'INSERT INTO user (username, password) VALUES (?, ?)',
-    #         (username, generate_password_hash(password))
-    #     )
-    #     db.commit()
-    #     return redirect(url_for('auth.login'))
+        # flash(error)
 
-    # flash(error)
+    return render_template('index.html')
 
-    return render_template('/.html')
+
+@app.route('/home',methods=('GET', 'POST'))
+def home():
+    return render_template("templates/home.html")
+
+@app.rout('/edit', methods=('GET', 'POST'))
+def edit():
+    return render_template("templates/edit.html")
+
