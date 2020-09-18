d5069c448e0989959605c71c71569705bfd79528
梅佳奕 <10165300206@stu.ecnu.edu.cn>
2020-09-18 14:41:50 +0800

    login()

--- app.py
+++ app.py
@@ -3,15 +3,32 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=('POST'))
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    # db = get_db()
+    # error = None
 
-    
-def create_app()
-    from . import models, routes, services
-    app = Flask(__name__)
-    models.init_app(app)
-    routes.init_app(app)
-    services.init_app(app)
-    return app
+    if not username:
+        error = 'Username is required.'
+    elif not password:
+        error = 'Password is required.'
+
+    ##############不管用户名密码是什么都login#############
+    # elif db.execute(
+    #     'SELECT id FROM user WHERE username = ?', (username,)
+    # ).fetchone() is not None:
+    #     error = 'User {} is already registered.'.format(username)
+
+    # if error is None:
+    #     db.execute(
+    #         'INSERT INTO user (username, password) VALUES (?, ?)',
+    #         (username, generate_password_hash(password))
+    #     )
+    #     db.commit()
+    #     return redirect(url_for('auth.login'))
+
+    # flash(error)
+
+    return render_template('/.html')
