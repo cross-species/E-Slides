4b2d5b62fe1f6f52de9c4fb17379e1154105a272
梅佳奕 <10165300206@stu.ecnu.edu.cn>
2020-09-18 12:51:25 +0800

    add startJupyter()

--- call.py
+++ call.py
@@ -6,6 +6,23 @@ def callMindMap_once(id, output, fileCat="html"):
         "." + fileCat + "\" --enable-mathjax --enable-prism --no-open " + id + ".md"
     os.system(order)
 
+def startJupyter():
+    order1 = "cd D:\\E-Slides\\"
+    order2 = "jupyter notebook"
+    status = False
+    if(not status):
+        os.system(order1)
+        os.system(order2)
+        status = True
+    return status
+
+# def callJupyter(code):
+    
+
+
+
+
+
 
 output = "test_out"
 fileCat = "txt"
